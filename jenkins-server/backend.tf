9400d9d1af50976e3baa0804c309c486f37ba51a
Amith P S <amithps0@gmail.com>
2024-10-04 16:25:35 +0530

    Update backend.tf

--- jenkins-server/backend.tf
+++ jenkins-server/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mytodoappbucket"
+    bucket = "mys3bucket1234567"
     key    = "jenkins/terraform.tfstate"
-    region = "us-east-2"
+    region = "ap-south-1"
   }
 }
